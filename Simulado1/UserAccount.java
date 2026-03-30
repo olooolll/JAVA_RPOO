6e832487af17de0daebee401681b7133eadc6431
lucas-ifsp <lucas.ruas@gmail.com>
2026-03-30 10:18:33 -0300

    feat: add review comments as TODOs

--- Simulado1/UserAccount.java
+++ Simulado1/UserAccount.java
@@ -10,6 +10,7 @@ public class UserAccount{
     private String email;
     private String userName;
     private Post[] post = new Post[MAX_POSTS];
+    // TODO na prova você NÃO deve usar ArrayList, mas array, como acima. ArrayList é uma lista, não um array.
     private ArrayList<UserAccount> follower= new ArrayList<UserAccount>();
     private int pointer = 0;
 
@@ -29,16 +30,16 @@ public class UserAccount{
 
     public void publish(String quote){
         Post post = new Post(this, quote);
-        this.updateTimeline(post);
+        this.updateTimeline(post); // TODO aqui você deve adicionar a sua lista de posts, não na sua timeline
         int size = this.follower.size();
-        for(int i=0; i<size; i++){
-            this.follower.get(i).updateTimeline(post);
+        for(int i=0; i<size; i++){ // TODO pode fazer o inline aqui i < follower.size() / followers.length
+            this.follower.get(i).updateTimeline(post); // TODO não precisa do this sempre, só quando o nome colide com algum nome de dentro do método ou parâmetro.
         }
     }
 
     public boolean delete(int postidx){
         if(this.post[postidx] != null){
-            this.post[postidx] = null;
+            this.post[postidx] = null; // TODO melhor limpar o array, ao invés de deixá-lo todo esburacado de null.
             return true;
         }
         return false;
@@ -48,7 +49,7 @@ public class UserAccount{
         String posts = "";
         for(int i=0; i<MAX_POSTS; i++){
             if(this.post[i] != null) {
-                posts += this.post[i].show() + "\n";
+                posts += this.post[i].show() + "\n"; // TODO você deve usar StringBuilder para evitar concatenação com + em loops
             }
         }
 
@@ -59,6 +60,7 @@ public class UserAccount{
         String posts = "";
         for(int i=0; i<MAX_POSTS; i++){
             if(this.post[i] != null) {
+                // TODO idem aqui. Além disso, se tá no seu array de posts, é porque é seu.
                 posts += (this.post[i].getAccount() == this) ? this.post[i].show() + "\n" : "";
             }
         }
@@ -71,6 +73,7 @@ public class UserAccount{
         int size = this.follower.size();
 
         for(int i=0; i<size; i++){
+            // TODO idem
             followers += "User: " + this.follower.get(i).getUserName() + " | E-mail: " + this.follower.get(i).getEmail() + "\n";
         }
 
@@ -78,20 +81,20 @@ public class UserAccount{
     }
 
     public void clapPost(int postIdx){
-        if(this.post[postIdx] != null) {
+        if(this.post[postIdx] != null) { // TODO tem que ver inclusive se o índice existe
             this.post[postIdx].clap();
         }
     }
 
     public void booPost(int postIdx){
-        if(this.post[postIdx] != null) {
+        if(this.post[postIdx] != null) { // TODO idem
             this.post[postIdx].boo();
         }
     }
 
     private void updateTimeline(Post newPost){
         this.post[pointer] = newPost;
-        pointer = (pointer == 9) ? 0 : (pointer += 1);
+        pointer = (pointer == 9) ? 0 : (pointer += 1); // TODO isso está errado. Olhe com cuidado para ver o porquê
     }
 
     public void acceptFollower(UserAccount newFollower){
